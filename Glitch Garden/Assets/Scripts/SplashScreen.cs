d042eb5922c271f9a3cb49f6ea0805b4535dc985
Phil Davidson <pxdavidson@msn.com>
2019-05-16 15:44:25 +0100

    Scenes, folder and prefab setup
    - Created 3 scenes (Game, Menu, Splash)
    - Created prefab folder and prefabbed everything in the scenes
    - Set up folder for all assets so far

---
+++ Glitch Garden/Assets/Scripts/SplashScreen.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SplashScreen : MonoBehaviour
+{
+    // Declare variables
+    [SerializeField] float loadingDelay = 5f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(LoadGameAfterDelay());
+    }
+
+    IEnumerator LoadGameAfterDelay()
+    {
+        yield return new WaitForSeconds(loadingDelay);
+        SceneManager.LoadScene("Menu");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
