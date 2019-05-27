9c046b57afdc034f0a9bf5db49448c7ad0f432dd
Phil Davidson <pxdavidson@msn.com>
2019-05-27 22:06:32 +0100

    Added UI buttons
    Added Button.cs that controls the state changes from button presses

---
+++ Glitch Garden/Assets/Scripts/Button.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Button : MonoBehaviour
+{
+    // Loads Game scene
+    public void LoadGameScene()
+    {
+        print("pressed");
+        SceneManager.LoadScene("Game");
+    }
+
+    // Quits game
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+}
