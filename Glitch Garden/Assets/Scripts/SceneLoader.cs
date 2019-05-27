e306835b603db1da78fce17851ee8e019fafd0f6
Phil Davidson <pxdavidson@msn.com>
2019-05-27 22:47:26 +0100

    Bug Fix & Canvas
    Replaced canvas in Menu
    Got rid of Button.cs and replaced with SceneLoader.cs
    Flow working from splash to game

---
+++ Glitch Garden/Assets/Scripts/SceneLoader.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneLoader : MonoBehaviour
+{
+
+    // Load game scene
+    public void LoadGameScene()
+    {
+        SceneManager.LoadScene("Game");
+    }
+
+    // Quit game
+    public void QuitTheGame()
+    {
+        Application.Quit();
+    }
+}
