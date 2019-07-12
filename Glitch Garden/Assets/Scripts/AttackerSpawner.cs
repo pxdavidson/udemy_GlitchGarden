627be34c661b6fa590cb98d77b05b929b360494a
Phil Davidson <pxdavidson@msn.com>
2019-07-12 21:27:49 +0100

    Add spawner and .cs script
    Was not finished, checking in so i can work on i on PC

---
+++ Glitch Garden/Assets/Scripts/AttackerSpawner.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AttackerSpawner : MonoBehaviour
+{
+    //Variables
+    bool spawnTrue = true;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    IEnumerator SpawnAttacker()
+    {
+        while (true)
+        {
+            debug.log("spawn");
+            
+        }
+    }
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
